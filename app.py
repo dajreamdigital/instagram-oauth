96c540ed72581aa4bdc96fc937c06072418fdf30
rarcega <rarcega@gmail.com>
2013-01-03 18:32:05 -0500

    Update app.py

--- app.py
+++ app.py
@@ -8,18 +8,20 @@ client = {
 	'client_id'      : '', 
 	'client_secret'  : '',
 	'grant_type'     : 'authorization_code', 
-	'redirect_uri'   : ''
+	'redirect_uri'   : '',
+	'auth_uri'       : 'https://api.instagram.com/oauth/authorize/',
+	'token_uri'      : 'https://api.instagram.com/oauth/access_token/'
 }
 
 @app.route('/')
 def root():
-	uri = 'https://api.instagram.com/oauth/authorize/?client_id={0}&redirect_uri={1}&response_type=code'.format(client['client_id'], client['redirect_uri'])
+	uri = client['auth_uri'] + '?client_id={0}&redirect_uri={1}&response_type=code'.format(client['client_id'], client['redirect_uri'])
 	return flask.redirect(uri)
 	
 @app.route('/auth')
 def auth():
 	client.update({'code': flask.request.args.get('code')})
-	return flask.Response(requests.post('https://api.instagram.com/oauth/access_token/', data=client).text, status=200, mimetype='application/json')
+	return flask.Response(requests.post(client['token_uri'], data=client).text, status=200, mimetype='application/json')
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
